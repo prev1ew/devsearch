5daa07f18bd23b37b7e0e8bdae173658f1b1f4d3
prev1ew <warcied1@gmail.com>
2022-02-27 16:21:45 +0200

    Initial commit

---
+++ devsearch/devsearch/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home_page(request):
+    return HttpResponse('Home Page')
+
+
+urlpatterns = [
+    path('', home_page),
+    path('admin/', admin.site.urls),
+    path('', include('projects.urls'))
+
+]
