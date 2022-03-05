2cfb23bdad5986cadaceb57e8633806b8ac2e145
Vitalii <warcied1@gmail.com>
2022-03-05 20:16:55 +0200

    templates edits

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-
+from .models import Project
 from django.http import HttpResponse
 
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+
+    projects_data = Project.objects.all()
+    context = {'projects': projects_data}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'projects/single_project.html')
+    project_obj = Project.objects.get(id=pk)
+    tags = project_obj.tags.all()
+    context = {'project': project_obj,
+               'tags': tags}
+    return render(request, 'projects/single_project.html', context)
