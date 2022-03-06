5bed9795468819cc32a04f9743726c230a27c631
Vitalii <warcied1@gmail.com>
2022-03-06 14:42:41 +0200

    basic CRUD added

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
 from django.http import HttpResponse
+from .forms import ProjectForm
 
 
 def projects(request):
@@ -16,3 +17,41 @@ def project(request, pk):
     context = {'project': project_obj,
                'tags': tags}
     return render(request, 'projects/single_project.html', context)
+
+
+def create_project(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def update_project(request, pk):
+    curr_project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=curr_project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=curr_project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def delete_project(request, pk):
+    curr_project = Project.objects.get(id=pk)
+
+    if request.method == 'POST':
+        curr_project.delete()
+        return redirect('projects')
+
+    context = {'object': curr_project}
+    return render(request, 'projects/delete_template.html', context)
