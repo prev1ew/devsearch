1b7ffc6a83197c7dc9fb6d315c0c9b954b0f0ccc
Vitalii <warcied1@gmail.com>
2022-03-12 16:12:03 +0200

    added users app

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -8,7 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     # path('', home_page),
     path('admin/', admin.site.urls),
-    path('', include('projects.urls'))
+    path('projects/', include('projects.urls')),
+    path('', include('users.urls')),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
