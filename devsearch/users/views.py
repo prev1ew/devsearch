b13f1d4b830367e6014eda15d814ddddc7c08ad9
Vitalii <warcied1@gmail.com>
2022-03-12 18:40:18 +0200

    render profiles

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
 
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles_data = Profile.objects.all()
+    context = {'profiles': profiles_data}
+    return render(request, 'users/profiles.html', context)
+
+
+def user_profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+
+    top_skills = profile.skill_set.exclude(description__exact='')
+    other_skills = profile.skill_set.filter(description='')
+
+    content = {'profile': profile,
+               'top_skills': top_skills,
+               'other_skills': other_skills}
+    return render(request, 'users/user-profile.html', content)
