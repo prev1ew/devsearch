bb88a9cf6c15523b1eadeeee68aedc556ad04846
Vitalii <warcied1@gmail.com>
2022-03-12 19:38:13 +0200

    added simple signals for 'Profile'

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-# Create your models here.
 
 
 class Profile(models.Model):
@@ -23,7 +22,7 @@ class Profile(models.Model):
                           primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
@@ -35,3 +34,6 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+
