72ed5dee03e9fe3814491f936e9c3f19bec0efcd
Vitalii <warcied1@gmail.com>
2022-03-13 18:03:07 +0200

    added registration page

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('profile/<str:pk>/', views.user_profile, name="user-profile"),
 
     path('login/', views.login_user, name='login'),
+    path('register/', views.register_user, name='register'),
     path('logout/', views.logout_user, name='logout'),
 ]
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from .models import Profile
 from django.contrib.auth.models import User
+from .forms import CustomUserCreation
 
 
 def profiles(request):
@@ -25,6 +26,9 @@ def user_profile(request, pk):
 
 def login_user(request):
 
+    page = 'login'
+    content = {'page': page}
+
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -45,10 +49,33 @@ def login_user(request):
         else:
             messages.error(request, 'Username or passwd is incorrect')
 
-    return render(request, 'users/login_register.html')
+    return render(request, 'users/login_register.html', content)
 
 
 def logout_user(request):
     logout(request)
     messages.info(request, 'User was successfully logged out')
     return redirect('login')
+
+
+def register_user(request):
+    page = 'register'
+    form = CustomUserCreation()
+
+    if request.method == 'POST':
+        form = CustomUserCreation(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created')
+
+            login(request, user)
+            return redirect('profiles')
+
+        else:
+            messages.error(request, 'An error was occurred during registration')
+
+    content = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', content)
